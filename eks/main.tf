9e41a3c7fc90d23c5ee8ebdab012be01e40fe999
akhil5672 <53939261+akhil5672@users.noreply.github.com>
2025-06-23 20:34:54 -0600

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -43,13 +43,3 @@ module "eks" {
   addons = var.addons
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
